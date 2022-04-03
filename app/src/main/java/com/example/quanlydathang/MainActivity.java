391838c6235a02329acc6e7928eb60e4172acf15
Duc Nguyen <ducnguyen201231@gmail.com>
2022-04-04 00:34:12 +0700

    bổ sung cột SDT vào bảng user

--- app/src/main/java/com/example/quanlydathang/MainActivity.java
+++ app/src/main/java/com/example/quanlydathang/MainActivity.java
@@ -6,9 +6,6 @@ import androidx.cardview.widget.CardView;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-
-import com.example.quanlydathang.database.CreateDatabase;
 import com.example.quanlydathang.ui.KhachHang.KhachHangActivity;
 
 public class MainActivity extends AppCompatActivity {
